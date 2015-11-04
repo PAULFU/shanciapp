236e9328d375472dcaa9ae0c065b80b8db34780b
付品友 <pinyoufu@outlook.com>
2015-11-04 11:16:56 +0800

    实现了recyclerview

---
+++ app/src/main/java/com/fupinyou/shanci/shanciapp/RecyclerHeaderViewHolder.java
@@ -0,0 +1,13 @@
+package com.fupinyou.shanci.shanciapp;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by yi on 2015/11/3.
+ */
+public class RecyclerHeaderViewHolder extends RecyclerView.ViewHolder {
+    public RecyclerHeaderViewHolder(View itemView) {
+        super(itemView);
+    }
+}
